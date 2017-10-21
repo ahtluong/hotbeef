568e839f47a4d52bb92669096fc836b44bf5cff2
Steven Eastcott <sdeastcott@crimson.ua.edu>
2017-10-21 15:39:25 -0500

    server stuff

--- server/controllers/user.js
+++
@@ -1,4 +0,0 @@
-var {mongoose} = require('../db/mongoose');
-var Router = require('express').Router;
-var app = new Router();
-module.exports.UserController = app; 
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://user:user@ds227555.mlab.com:27555/hotbeef', { useMongoClient: true }).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));;
 
 /*
 mongoose.connect(config.database, {
