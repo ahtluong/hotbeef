6700dee46c329a67d65f331084019c78eef15877
emilykimhuynh <emilyhuynh101@gmail.com>
2017-10-21 17:32:45 -0500

    w0w things work now

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -7,8 +7,9 @@ module.exports.ProfileController = app;
 
 app.get('/inventory/:ingredient', (req, res) => {
   let ingredient = req.params.ingredient;
+  console.log(ingredient);
 
-  IngredientMap.find({ingredient : {$exists: true}}).then((inventory) => {
+  IngredientMap.find({'name' : ingredient}).then((inventory) => {
     res.send({inventory});
   }, (e) => {
     res.status(400).send(e);
@@ -17,11 +18,14 @@ app.get('/inventory/:ingredient', (req, res) => {
 
 
 app.post('/inventory', (req, res) => {
-  console.log(req)
+  console.log(req.body);
+
   let ingredient = new IngredientMap(req.body);
+  console.log(ingredient);
 
   ingredient.save().then((doc) => {
     res.send(doc);
+    console.log(doc.body);
   }, (e) => {
     res.status(400).send(e);
   });
