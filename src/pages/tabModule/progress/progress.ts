e9b11fd331c26bde73090393c0b7f9985944edc8
emilykimhuynh <emilyhuynh101@gmail.com>
2017-10-22 08:59:24 -0500

    fix progress bar

--- src/pages/tabModule/progress/progress.ts
+++ src/pages/tabModule/progress/progress.ts
@@ -9,44 +9,9 @@ import { UserProvider } from '../../../providers/user/user';
 })
 export class ProgressPage {
 
-  username = 'emily';
-  // dishNames = [];
-  // dishPercentages = [];
-  dishes = [];
-
   constructor(public navCtrl: NavController, public navParams: NavParams, private userProvider: UserProvider) {
   }
   
   ionViewDidLoad() {
-    this.userProvider.getUserInventory(this.username).then(data => {
-
-      // for each dish
-      for (var obj in data) {
-        // this.dishNames.push(data[obj].dish_name);
-        var dish = {}
-        dish['name'] = data[obj].dish_name;
-        var count = 0;
-        var ingredients = data[obj].ingredients;
-
-        // for each ingredient in the dish
-        for (var item in ingredients) {
-          if (ingredients[item].acquired == true) {
-            count += 1;
-          }
-        }
-
-        // calculate the percentage
-        var percentage = ((count * 1.0) / (ingredients.length * 1.0)) * 100
-        // this.dishPercentages.push(percentage);
-        dish['amount'] = percentage;
-        console.log(dish['amount']);
-        this.dishes.push(dish);
-        console.log(this.dishes);
-      }
-    });
-  }
-
-  setAmount(amt) {
-    console.log('jk');
   }
 }
