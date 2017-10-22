194220009ef716d44252979d8c566ebf51d316c7
Steven Eastcott <sdeastcott@crimson.ua.edu>
2017-10-22 00:03:08 -0500

    need username from header and not body

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -16,7 +16,7 @@ app.get('/inventory/:ingredient', (req, res) => {
 
 app.put('/select_ingredient', (req, res) => {
   let ingredient = req.body.ingredient;
-  let username = req.body.username;
+  let username = req.headers.username;
   
   User.update({'username': username}, {$set: {starter_ingredient: ingredient}}).then((count, status) => {
     IngredientMap.find({name: ingredient}).then((ingredient_map) => {
@@ -37,7 +37,7 @@ app.get('/all_ingredient_list', (req, res) => {
 
 app.put('/bio', (req, res) => {
   let bio = req.body.bio;
-  // TODO: let username = req.
+  let username = req.headers.username;
   User.update({'username': username}, {$set: {bio: bio}}).then((count, status) => {}, (e) => {});
 });
 
@@ -51,21 +51,59 @@ app.post('/inventory', (req, res) => {
   });
 });
 
-// TODO: IN PROGRESS
 app.post('/rightswipe', (req, res) => {
   let user = req.body.user; 
   let user_swipes = user.swipes;
   let username = user.username;
+  let user_ingredient = user.starter_ingredient;
+
   let other_user = req.body.other_user;
-  let other_user_swipes = other_user.swipes
-  let i = other_user.length - 1;
+  let other_user_swipes = other_user.swipes;
+  let other_user_name = other_user.username;
+  let other_user_ingredient = other_user.starter_ingredient;
+
+  let j = user_swipes.length - 1;
+  let i = other_user_swipes.length - 1;
 
   while (i >= 0) {
-    if (username == other_user[i].other_user) {
-      otherUserSwipes.splice(i, 1);
-    }
+    // If I'm in their swipes...
+    if (username == other_user_swipes[i].other_user) {
+      // Remove me from their swipes
+      other_user_swipes.splice(i, 1);
+      while (j >= 0) {
+        if (other_user_name == user_swipes[j].other_user) {
+          // Remove them from my swipes
+          user_swipes.splice(j, 1);
+        }
+        j--;
+      }
+
+      // Update my ingredients list
+      let dishes = user.inventory;
+      dishes.forEach(function(dish) {
+        dish.ingredients.forEach(function(ingredient) {
+          if (ingredient == other_user_ingredient) {
+            ingredient = true;
+          }
+        })
+      });
+
+      // Update their ingredients list
+      dishes = other_user.inventory;
+      dishes.forEach(function(dish) {
+        dish.ingredients.forEach(function(ingredient) {
+          if (ingredient == user_ingredient) {
+            ingredient = true;
+          }
+        })
+      });
+      break;
+    } 
     i--;
   }
+
+  res.status(200);
 });
+
 
 module.exports.UserController = app;
